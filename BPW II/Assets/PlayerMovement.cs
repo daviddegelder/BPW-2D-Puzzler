bcffe87db0d4ed7048ad38a5a2627a4477b060bc
David de Gelder <davidosodegelder@gmail,com>
2024-02-29 18:34:03 +0100

    Add player raycast collision

--- BPW II/Assets/PlayerMovement.cs
+++ BPW II/Assets/PlayerMovement.cs
@@ -1,39 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
  public class PlayerMovement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+ {
+     public Tilemap groundTilemap;
+     public Tilemap collisionTilemap;
+     
     void Update()
     {
         
-        if (Input.GetKeyDown("w"))
-        {
-            MovePlayer(Vector3.up);
-        }
-        if (Input.GetKeyDown("a"))
-        {
-            MovePlayer(Vector3.left);
-        }
-        if (Input.GetKeyDown("s"))
-        {
-            MovePlayer(Vector3.down);
-        }
-        if (Input.GetKeyDown("d"))
-        {
-            MovePlayer(Vector3.right);
-        }
+        if (Input.GetKeyDown("w")) { MovePlayer(Vector3.up); }
+        if (Input.GetKeyDown("a")) { MovePlayer(Vector3.left); }
+        if (Input.GetKeyDown("s")) { MovePlayer(Vector3.down); }
+        if (Input.GetKeyDown("d")) { MovePlayer(Vector3.right); }
     }
 
     void MovePlayer(Vector3 direction)
+    {   
+        if (CanMove(direction))
+        {
+            transform.Translate(direction);
+        }
+        
+    }
+
+    bool CanMove(Vector3 direction)
     {
-        transform.Translate(direction);
+        Vector3Int gridPosition = groundTilemap.WorldToCell(transform.position + direction);
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, 1f);
+
+        if (hit.collider != null)
+        {
+            return false;
+        }
+        if (!groundTilemap.HasTile(gridPosition))
+        {
+            return false;
+        }
+        // if (collisionTilemap.HasTile(gridPosition))
+        // {
+        //     return false;
+        // }
+
+        return true;
     }
 }
